3fdb39e2b5ea2d0d9aba78eafa59224b5fecf581
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2025-10-07 22:20:08 -0700

    Project Init w/ tailwind, shadcn ui, vite pwa + assets

---
+++ pwa-assets.config.ts
@@ -0,0 +1,33 @@
+import {
+  defineConfig,
+  Preset,
+} from '@vite-pwa/assets-generator/config'
+
+export const preset: Preset = {
+  transparent: {
+    sizes: [64, 192, 512],
+    favicons: [[48, 'favicon.ico']],
+  },
+  maskable: {
+    sizes: [512],
+    resizeOptions: {
+        background: "#050706",
+        fit: "contain"
+    },
+  },
+  apple: {
+    sizes: [180],
+    resizeOptions: {
+        background: "#050706",
+        fit: "contain"
+    },
+  },
+}
+
+export default defineConfig({
+  headLinkOptions: {
+    preset: '2023',
+  },
+  preset,
+  images: ['public/logo.svg']
+})
---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { VitePWA, type ManifestOptions } from 'vite-plugin-pwa'
+
+const manifest: Partial<ManifestOptions> = {
+  name: "SMV Telemetry",
+  short_name: "SMVT",
+  description: "Driver dashboard & Telemetry System for CalPoly's SMV Team",
+  theme_color: "#050706",
+  background_color: "#050706",
+  // From `pnpm run generate-pwa-assets`
+  "icons": [
+    {
+      "src": "pwa-64x64.png",
+      "sizes": "64x64",
+      "type": "image/png"
+    },
+    {
+      "src": "pwa-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "pwa-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    },
+    {
+      "src": "maskable-icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "maskable"
+    }
+  ]
+}
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest
+    })
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
